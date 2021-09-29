edda4716cc9545f862a3e0a1f805e34c9841cc9d
Evan Mesterhazy <etm2131@columbia.edu>
2021-09-28 20:05:09 -0400

    fix: Wait for event to avoid busy looping
    Prior to this commit the game busy loops and eats up 100% of the CPU
    core it is running on.

--- src/diffusion_simulator.py
+++ src/diffusion_simulator.py
@@ -44,29 +44,30 @@ class DiffusionSimulator:
 
             pygame.display.flip() # Update the display
 
-            for event in pygame.event.get():
-                if event.type == KEYDOWN and event.key == K_ESCAPE or event.type == QUIT:
-                    self.running = False
-                elif event.type == KEYDOWN:
-                    if event.key == K_RETURN:
-                        self.grid.diffuse()
-                    elif event.key == K_r:
-                        self.choice_sq.choose_red()        
-                    elif event.key == K_g:
-                        self.choice_sq.choose_green()        
-                    elif event.key == K_b:
-                        self.choice_sq.choose_blue()    
-                    elif event.key == K_x:
-                        self.choice_sq.choose_blocked()  
-                    elif event.key == K_BACKSPACE:
-                        self.grid.clear_chemicals()    
-                elif event.type == MOUSEBUTTONDOWN:
-                    if self.choice_sq.choice == None:
-                        self.grid.click(pygame.mouse.get_pos(), self.choice_sq.choice)
-                    else:
-                        self.mouse_pressed = True
-                elif event.type == MOUSEBUTTONUP:
-                    self.mouse_pressed = False
+            # Wait for the next event to avoid busy looping
+            event = pygame.event.wait()
+            if event.type == KEYDOWN and event.key == K_ESCAPE or event.type == QUIT:
+                self.running = False
+            elif event.type == KEYDOWN:
+                if event.key == K_RETURN:
+                    self.grid.diffuse()
+                elif event.key == K_r:
+                    self.choice_sq.choose_red()        
+                elif event.key == K_g:
+                    self.choice_sq.choose_green()        
+                elif event.key == K_b:
+                    self.choice_sq.choose_blue()    
+                elif event.key == K_x:
+                    self.choice_sq.choose_blocked()  
+                elif event.key == K_BACKSPACE:
+                    self.grid.clear_chemicals()    
+            elif event.type == MOUSEBUTTONDOWN:
+                if self.choice_sq.choice == None:
+                    self.grid.click(pygame.mouse.get_pos(), self.choice_sq.choice)
+                else:
+                    self.mouse_pressed = True
+            elif event.type == MOUSEBUTTONUP:
+                self.mouse_pressed = False
 
             if self.mouse_pressed:
                 self.grid.click(pygame.mouse.get_pos(), self.choice_sq.choice)
